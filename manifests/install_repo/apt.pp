2600207e1ef6738edc0262ca6a8ef89b3b230b25
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-14 07:39:05 +0000

    adattato al nuovo modulo apt

--- manifests/install_repo/apt.pp
+++ manifests/install_repo/apt.pp
@@ -5,28 +5,22 @@
 #
 class fluentd::install_repo::apt () {
 
-  $location = $::lsbdistcodename? {
-    'precise' => $::lsbdistcodename,
-    'lucid'   => 'debian'
+  # Sorry for the different naming of the Rpository between debian and redhat.
+  # But I dont want rename it to avoid a duplication.
+  apt_puppetlabs::source { 'treasure-data':
+    location    => "http://packages.treasure-data.com/debian",
+    release     => "lucid",
+    repos       => "contrib",
+    include_src => false,
   }
 
-    # Sorry for the different naming of the Rpository between debian and redhat.
-    # But I dont want rename it to avoid a duplication.
-
-    file { '/tmp/packages.treasure-data.com.key':
-        ensure => file,
-        source => 'puppet:///modules/fluentd/packages.treasure-data.com.key'
-    }->
-    exec { "import gpg key Treasure Data":
-        command => "/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -",
-        unless  => "/usr/bin/apt-key list | grep -q 'Treasure Data'",
-    } ->
-
-    apt::source { 'treasure-data':
-      location    => "http://packages.treasure-data.com/${location}",
-      release     => $::lsbdistcodename,
-      repos       => "contrib",
-      include_src => false,
-      notify      => Exec['apt-get_update']
-    }
+  file { '/tmp/packages.treasure-data.com.key':
+    ensure => file,
+    source => 'puppet:///modules/fluentd/packages.treasure-data.com.key'
+  }->
+  exec { "import gpg key Treasure Data":
+    command => "/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -",
+    unless  => "/usr/bin/apt-key list | grep -q 'Treasure Data'",
+    notify  => Class['::apt_puppetlabs::update'],
+  }
 }
